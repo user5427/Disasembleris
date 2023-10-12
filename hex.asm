8f49d18f3ae072d95234b8c353070b46fcf13cc3
TheBlueBrain <62613378+TheBlueBrain@users.noreply.github.com>
2023-10-12 16:27:05 +0300

    komentaru pridejimas

--- hex.asm
+++ hex.asm
@@ -1,39 +1,38 @@
 .model small
 .stack 100h
 .data
-	x db 170
-	buff db 2 dup(0), 24h
+	x db 170	;skaicius kuri paversim i hex
+	buff db 2 dup(0), 24h	;outputo bufferis
 .code
 start:
 
-
 	mov ax, @data
-	mov ds, ax
-	mov cx, 2
-	mov di, 1
-	mov al, x
+	mov ds, ax	;data segmento init
+	mov cx, 2	;1 baite esanti skaiciu konvertavus i hex uzims max 2 simbolius
+	mov di, 1	;destination index init
+	mov al, x	;skaiciaus laikymas (Xh&Xl 1B Xx 2B)
 	l1:
-		mov ah, al
-		and ah, 15
-		cmp ah, 10
+		mov ah, al	
+		and ah, 15	;paimami paskutiniai 4 bitai
+		cmp ah, 10	;jei >10 tada skaiciu reikia paversti i simboli kitu atveju lieka skaicius
 		jge hex
-		add ah, 48
-		mov (buff+di), ah
-		dec di
-		shr al, 4
+		add ah, 48	;hex 0 
+		mov (buff+di), ah; i outputa irasomas skaicius
+		dec di	;pakeiciama rasymo pozicija
+		shr al, 4	;is al panaikinami paskutiniai 4 bitai
 	loop l1
 	jmp fin
 	hex:; 10 arba daugiau
 		add ah, 55	;pridejus 10 gaunasi ascii A
-		mov(buff+di), ah
-		dec di
+		mov(buff+di), ah	;irasoma raide
+		dec di;taspats kas virsuje tik reikia paciam patikrinti ar iseiti is loop ir sumazinti counteri
 		shr al, 4
 		dec cx
 		cmp cx, 0
 		je fin
 		jmp l1
 	fin:
-		mov ah, 9
+		mov ah, 9	;rasymas ir programos uzdarymas
 		mov dx, offset buff
 		int 21h
 		mov ax, 4c00h

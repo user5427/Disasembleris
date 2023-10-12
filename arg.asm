91b1ed014a727879337d3483b68253401b799152
TheBlueBrain <62613378+TheBlueBrain@users.noreply.github.com>
2023-10-12 16:40:09 +0300

    komentarai

--- arg.asm
+++ arg.asm
@@ -5,18 +5,18 @@
 .code
 start:
 	push @data
-	pop ds
-	mov si, 81h
+	pop ds	;;duomenu segmento init
+	mov si, 81h	;cia laikomi commandline argumentai
 	mov di, offset buff
 	xor cx, cx
-	mov cl, es:[80h]
+	mov cl, es:[80h] ;offsetu 80 laikomas argumento simboliu kiekis
 	l:
-		mov al, es:[si] 
-		mov [di], al
+		mov al, es:[si] ;i al irasomas C-L simbolis
+		mov [di], al; simbolis irasomas i bufferi
 		inc si
 		inc di
 	loop l
-	mov ax, 4000h
+	mov ax, 4000h;rasymas i stdout
 	mov bx, 1
 	mov dx, offset buff
 	xor cx, cx
